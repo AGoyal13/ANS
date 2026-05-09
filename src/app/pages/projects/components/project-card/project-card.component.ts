5d9e3c1aeaa6a0b037d960b55514b18eb2701320
AGoyal13 <95077957+AGoyal13@users.noreply.github.com>
2026-05-09 16:09:20 +0530

    Add files via upload

--- src/app/pages/projects/components/project-card/project-card.component.ts
+++ src/app/pages/projects/components/project-card/project-card.component.ts
@@ -1,8 +1,10 @@
 import { Component, input, signal } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { Project } from '../../../../shared/models/project.model';
 
 @Component({
   selector: 'ans-project-card',
+  imports: [DatePipe],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss',
 })
